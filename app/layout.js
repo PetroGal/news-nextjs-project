a3216db76ecc434ae318ec5666d44fd1b9115dd5
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-06-08 18:31:03 +0300

    created main-header component

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,15 @@
-import './globals.css';
+import MainHeader from "@/components/MainHeader/page.js"
+import "./globals.css"
 
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
 }
 
 export default function RootLayout({ children }) {
- return (
-    <html lang="en">
+  return (
+    <html lang='en'>
+      <MainHeader />
       <body>{children}</body>
     </html>
   )
