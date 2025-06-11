5b02c2955c2232727b3138f1318e2b1f94566eaf
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-06-11 21:22:36 +0300

    ensured uploading images on the news page dynamically

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,22 @@
 import Link from "next/link.js"
+import { DUMMY_NEWS } from "@/dummy-news.js"
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
       <ul className='news-list'>
-        <li>
-          <Link href='/news/first-news'>First News</Link>
-        </li>
-        <li>
-          <Link href='/news/second-news'>Second News</Link>
-        </li>
-        <li>
-          <Link href='/news/third-news'>Third News</Link>
-        </li>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   )
