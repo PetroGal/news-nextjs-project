4918ad1ce6eb63e4a5024d9d8d5aad06caf16e16
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-06-11 21:39:28 +0300

    updated NewsDetailsPage adding the details dynamically

---
+++ app/news/[slug]/page.js
@@ -0,0 +1,16 @@
+import { DUMMY_NEWS } from "@/dummy-news.js"
+
+export default function NewsDetailPage({ params }) {
+  const newsSlug = params.slug
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug)
+  return (
+    <article className='news-article'>
+      <header>
+        <h1>{newsItem.title}</h1>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  )
+}
