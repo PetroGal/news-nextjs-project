20cac5314dbeaceb19cc1a863e5ab43c564e4a1c
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-06-19 21:24:15 +0300

    initial functionality of a year identifier

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,3 +1,16 @@
+import { getAvailableNewsYears } from "@/lib/news.js"
+
 export default function ArchivePage() {
-  return
+  const links = getAvailableNewsYears()
+
+  return (
+    <header id='archive-header'>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
