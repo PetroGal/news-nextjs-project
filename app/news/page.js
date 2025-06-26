3917c3d6c8530eac0c86330fa2b21a9e1009ec91
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-06-26 15:31:22 +0300

    created NewsList component

--- app/news/page.js
+++ app/news/page.js
@@ -1,23 +1,11 @@
-import Link from "next/link.js"
 import { DUMMY_NEWS } from "@/dummy-news.js"
+import NewsList from "@/components/news-list.js"
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className='news-list'>
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img
-                src={`/images/news/${newsItem.image}`}
-                alt={newsItem.title}
-              />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   )
 }
